1bb6490627568ca40e8da12ba59443db6d66b3dc
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-29 15:52:48 -0300

    fix(user.proxy.ts): removed the "password" property from the proxy class

--- src/modules/user/models/user.proxy.ts
+++ src/modules/user/models/user.proxy.ts
@@ -2,13 +2,11 @@ import { UserEntity } from '../entities/user.entity'
 
 export class UserProxy {
     public email: string
-    public password: string
     public phone: string
     public roles: string
 
     public constructor(entity: UserEntity) {
         this.email = entity.email
-        this.password = entity.password
         this.phone = entity.phone
         this.roles = entity.roles
     }
