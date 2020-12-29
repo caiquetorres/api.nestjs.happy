6e359bc5add8915b57b7794f0985326a0145af8a
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-29 11:36:17 -0300

    feat(auth.module.ts): added "AuthController" in the "AuthModule" controllers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,7 +4,10 @@ import { AuthService } from './services/auth.service'
 import { JwtStrategyService } from './services/jwt.strategy.service'
 import { LocalStrategyService } from './services/local.strategy.service'
 
+import { AuthController } from './controllers/auth.controller'
+
 @Module({
+    controllers: [AuthController],
     providers: [AuthService, JwtStrategyService, LocalStrategyService],
     exports: [AuthService]
 })
