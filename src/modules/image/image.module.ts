c8b45bb8e44fc4522d2ac5bb322017aedea0dd68
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-29 18:09:31 -0300

    feat(image.module.ts): set it "imports", "controllers", "providers" and "exports"

--- src/modules/image/image.module.ts
+++ src/modules/image/image.module.ts
@@ -1,4 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
-@Module({})
+import { ImageEntity } from './entities/image.entity'
+
+import { ImageService } from './services/image.service'
+
+import { ImageController } from './controllers/image.controller'
+
+@Module({
+    imports: [TypeOrmModule.forFeature([ImageEntity])],
+    controllers: [ImageController],
+    providers: [ImageService],
+    exports: [ImageService]
+})
 export class ImageModule {}
