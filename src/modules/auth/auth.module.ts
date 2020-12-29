a6eb4c0996eb3876fa2cd71584d2c0f5255fe156
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-29 11:25:09 -0300

    feat(auth.module.ts): added the "JwtStrategyService" and the "LocalStrategyService" in the "AuthModule" imports

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 
 import { AuthService } from './services/auth.service'
+import { JwtStrategyService } from './services/jwt.strategy.service'
+import { LocalStrategyService } from './services/local.strategy.service'
 
 @Module({
-    providers: [AuthService],
+    providers: [AuthService, JwtStrategyService, LocalStrategyService],
     exports: [AuthService]
 })
 export class AuthModule {}
