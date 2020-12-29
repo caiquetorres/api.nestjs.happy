ac84c2bb02e29907917d19ee342bf695346da690
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-29 17:55:58 -0300

    feat(crud.ts): created more function to allow mapping any type of data that will be passed in the "mapCrud" function

--- src/utils/crud.ts
+++ src/utils/crud.ts
@@ -3,24 +3,83 @@ import { GetManyDefaultResponse } from '@nestjsx/crud'
 import { ToProxy } from 'src/common/to-proxy'
 
 /**
- * Function that can convert all the entities that were being passed through
- * the "GetManyDefaultResponse" class instance in it respective proxy
- * @param getManyDefaultResponse stores the complete data that is returned
- * from the service class
+ * Function that can convert all the entities that were being passed in their
+ * respective proxies
+ * @param value stores the complete data that is returned from the service
+ * class
  * @param params stores the default params that, if exists, will be passed
  * in the "ToProxy" method params parameter
  */
-export function mapCrud<TProxy, TEntity extends ToProxy<TProxy>>(
-    getManyDefaultResponse: GetManyDefaultResponse<TEntity> | TEntity[],
+export function mapCrud<
+    TEntity extends ToProxy<ReturnType<TEntity['toProxy']>>
+>(
+    value: TEntity | TEntity[],
     ...params: Parameters<TEntity['toProxy']>
-): GetManyDefaultResponse<TProxy> | TProxy[] {
-    if (Array.isArray(getManyDefaultResponse))
-        return getManyDefaultResponse.map(entity => entity.toProxy(...params))
+): ReturnType<TEntity['toProxy']> | ReturnType<TEntity['toProxy']>[]
 
-    return {
-        ...getManyDefaultResponse,
-        data: getManyDefaultResponse.data.map(entity =>
-            entity.toProxy(...params)
-        )
-    }
+/**
+ * Function that can convert all the entities that were being passed in their
+ * respective proxies
+ * @param value stores the complete data that is returned from the service
+ * class
+ * @param params stores the default params that, if exists, will be passed
+ * in the "ToProxy" method params parameter
+ */
+export function mapCrud<
+    TEntity extends ToProxy<ReturnType<TEntity['toProxy']>>
+>(
+    value: TEntity[] | GetManyDefaultResponse<TEntity>,
+    ...params: Parameters<TEntity['toProxy']>
+):
+    | ReturnType<TEntity['toProxy']>[]
+    | GetManyDefaultResponse<ReturnType<TEntity['toProxy']>>
+
+/**
+ * Function that can convert all the entities that were being passed in their
+ * respective proxies
+ * @param value stores the complete data that is returned from the service
+ * class
+ * @param params stores the default params that, if exists, will be passed
+ * in the "ToProxy" method params parameter
+ */
+export function mapCrud<
+    TEntity extends ToProxy<ReturnType<TEntity['toProxy']>>
+>(
+    value: TEntity | TEntity[] | GetManyDefaultResponse<TEntity>,
+    ...params: Parameters<TEntity['toProxy']>
+):
+    | ReturnType<TEntity['toProxy']>
+    | ReturnType<TEntity['toProxy']>[]
+    | GetManyDefaultResponse<ReturnType<TEntity['toProxy']>> {
+    if (Array.isArray(value))
+        return value.map(entity => entity.toProxy(...params))
+
+    if (isGetMany(value))
+        return {
+            ...value,
+            data: value.data.map(entity => entity.toProxy(...params))
+        }
+
+    return value.toProxy()
+}
+
+/**
+ * Function that checks if the "value" parameter is of the type
+ * "GetManyDefautlResponse"
+ * @param value stores the data that will be validated
+ */
+export function isGetMany<
+    TEntity extends ToProxy<ReturnType<TEntity['toProxy']>>
+>(
+    value: TEntity | TEntity[] | GetManyDefaultResponse<TEntity>
+): value is GetManyDefaultResponse<TEntity> {
+    return hasDataProperty(value) && Array.isArray(value.data)
+}
+
+/**
+ * Function that checks if the "value" parameter has the "data" property
+ * @param value stores the data that will be validated
+ */
+export function hasDataProperty(value: unknown): value is { data: unknown[] } {
+    return value.hasOwnProperty('data')
 }
