7cfebb2edac56ff1bafd435a2f36e364fd1980d4
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-29 10:01:23 -0300

    feat(auth.module.ts & image.module.ts & orphanage.module.ts & user.module.ts): created those files and added them in the app module class

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common'
+import { AuthModule } from './modules/auth/auth.module'
+import { UserModule } from './modules/user/user.module';
+import { ImageModule } from './modules/image/image.module';
+import { OrphanageModule } from './modules/orphanage/orphanage.module';
 
-@Module({})
+@Module({
+    imports: [AuthModule, UserModule, ImageModule, OrphanageModule]
+})
 export class AppModule {}
