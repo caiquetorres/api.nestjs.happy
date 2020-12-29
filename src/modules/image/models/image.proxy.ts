145f58afa78cb7fde31e90c127fafddbfc403e1e
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-29 18:49:04 -0300

    feat(image.proxy.ts): removed the "orphanage" property

--- src/modules/image/models/image.proxy.ts
+++ src/modules/image/models/image.proxy.ts
@@ -1,17 +1,11 @@
 import { ImageEntity } from '../entities/image.entity'
 
-import { OrphanageProxy } from 'src/modules/orphanage/models/orphanage.proxy'
-
 export class ImageProxy {
     public id: number
     public imageUrl: string
-    public orphanage?: OrphanageProxy
 
     public constructor(entity: ImageEntity) {
         this.id = entity.id
         this.imageUrl = entity.imageUrl
-        this.orphanage = entity.orphanage
-            ? entity.orphanage.toProxy()
-            : undefined
     }
 }
