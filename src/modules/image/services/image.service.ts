90238b766d9f27af2a3738947f53fe04c248b3da
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-29 16:41:09 -0300

    feat(image.service.ts): finished the logic of the methods "create", "listOne", "listMany" and "delete"

--- src/modules/image/services/image.service.ts
+++ src/modules/image/services/image.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { GetManyDefaultResponse } from '@nestjsx/crud'
+import { CrudRequest, GetManyDefaultResponse } from '@nestjsx/crud'
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm'
 import { Repository } from 'typeorm'
 
 import { ImageEntity } from '../entities/image.entity'
+import { OrphanageEntity } from 'src/modules/orphanage/entities/orphanage.entity'
 
+import { CreateImagePayload } from '../models/create-image.payload'
 import { ImageProxy } from '../models/image.proxy'
+
+import * as DefaultValidationMessages from '../../../models/default-validation-messages'
 
 @Injectable()
 export class ImageService extends TypeOrmCrudService<ImageEntity> {
@@ -17,19 +21,51 @@ export class ImageService extends TypeOrmCrudService<ImageEntity> {
         super(repository)
     }
 
-    public async create(): Promise<ImageProxy> {
-        return null
+    public async create(
+        saveImagePayload: CreateImagePayload
+    ): Promise<ImageEntity | ImageEntity[]> {
+        const { orphanageId, imagesUrl } = saveImagePayload
+
+        const orphanage = await OrphanageEntity.findOne({
+            id: orphanageId
+        })
+        if (!orphanage)
+            throw new NotFoundException(
+                DefaultValidationMessages.entityNotFound(orphanageId)
+            )
+
+        return this.repository.save(
+            imagesUrl.map<Partial<ImageEntity>>(imageUrl => ({
+                imageUrl,
+                orphanage
+            }))
+        )
     }
 
-    public async listOne(): Promise<ImageProxy> {
-        return null
+    public async listOne(imageId: number): Promise<ImageProxy> {
+        const entity = await ImageEntity.findOne({ id: imageId })
+
+        if (!entity)
+            throw new NotFoundException(
+                DefaultValidationMessages.entityNotFound(imageId)
+            )
+
+        return entity
     }
 
-    public async listMany(): Promise<GetManyDefaultResponse<ImageProxy>> {
-        return null
+    public async listMany(
+        crudRequest: CrudRequest
+    ): Promise<GetManyDefaultResponse<ImageProxy> | ImageProxy[]> {
+        return this.getMany(crudRequest)
     }
 
-    public async delete(): Promise<void> {
-        return null
+    public async delete(imageId: number): Promise<void> {
+        const existsImage = await OrphanageEntity.exists(imageId)
+        if (!existsImage)
+            throw new NotFoundException(
+                DefaultValidationMessages.entityNotFound(imageId)
+            )
+
+        await ImageEntity.delete({ id: imageId })
     }
 }
