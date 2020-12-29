358215681886c08fc9a84a166aef1a1186c17717
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-29 10:54:41 -0300

    feat(login.payload.ts & token.proxy.ts): created those files

---
+++ src/modules/auth/models/login.payload.ts
@@ -0,0 +1,4 @@
+export class LoginPayload {
+    public email: string
+    public password: string
+}
---
+++ src/modules/auth/models/token.proxy.ts
@@ -0,0 +1,7 @@
+export class TokenProxy {
+    public token: string
+
+    constructor(token: string) {
+        this.token = token
+    }
+}
