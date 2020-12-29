79ead596e9983ac66dc73ceae45c64c5fba8d864
caiquetorres <caiquetorres.bastos@hotmail.com>
2020-12-29 10:37:17 -0300

    feat(user.entity.ts): created this files

---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,43 @@
+import { BaseEntity } from 'src/common/base-entity'
+import { Column, Entity } from 'typeorm'
+import { UserProxy } from '../models/user.proxy'
+import { ToProxy } from '../../../common/to-proxy'
+
+@Entity('users')
+export class UserEntity extends BaseEntity implements ToProxy<UserProxy> {
+    public constructor(partial: Partial<UserEntity>) {
+        super()
+        Object.assign(this, partial)
+    }
+
+    @Column({
+        type: 'varchar',
+        length: 30,
+        nullable: false
+    })
+    public email: string
+
+    @Column({
+        type: 'varchar',
+        nullable: false
+    })
+    public password: string
+
+    @Column({
+        type: 'varchar',
+        length: 30,
+        nullable: false
+    })
+    public phone: string
+
+    @Column({
+        type: 'varchar',
+        length: 20,
+        nullable: true
+    })
+    public roles: string
+
+    public toProxy(): UserProxy {
+        return new UserProxy(this)
+    }
+}
